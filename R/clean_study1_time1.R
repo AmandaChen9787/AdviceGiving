1269276a590cede759cf245207b4048c0e055586
陈再丹 <chenzaidan@chenzaidans-MacBook-Air-2.local>
2024-09-20 12:12:04 -0400

    Update all datasets

--- R/clean_study1_time1.R
+++ R/clean_study1_time1.R
@@ -9,10 +9,11 @@ Advisee <- data.table::fread(here::here("Data/Raw Data/S1_time1.csv"),
   dplyr::mutate(Guess = coalesce(Guess, Guess_L)) %>%
   dplyr::mutate(Guess = ifelse(Guess == "Compete against the High Performer Group",1,0)) %>%
   dplyr::select(PROLIFIC_PID,Guess,default,Score,Age,Gender, Ethnicity,Education) %>%
+  dplyr::rename("ProlificID" = "PROLIFIC_PID") %>%
   dplyr::mutate(Score = as.numeric(Score))
 
 
 saveRDS(Advisee, file = here::here("Data/Study1_time1.Rds"))
-Advisee %>% write.csv("Data/Cleaned Data/S1_time1_cleaned.csv")
+Advisee %>% write.csv("Data/Cleaned Data/S1_time1_clean.csv")
 
 
